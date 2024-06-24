12841c3467a43d88d562f206d332c0747cc57e01
NoRi-230401 <sedia2011-apple@yahoo.co.jp>
2024-06-25 08:50:58 +0900

    dev202

--- examples/ImageViewer/main.cpp
+++ examples/ImageViewer/main.cpp
@@ -2,6 +2,23 @@
 #include "ImageViewer.hpp"
 // clang-format on
 
+#define MDM2   -2    // init
+#define MDM1   -1    // setup start
+#define MD00   0     // setup don (Normal mode)
+#define MD01   1     // MD01  FileSystem change
+#define MD02   2     // MD02  load menu.bin
+#define MD03   3     // MD03  save bin to SD
+#define MD04   4     // MD04  power off
+#define MD_END 4     //
+int MODE_ST = MDM2;  // mode status
+extern void MDxx_BtnChk();
+extern void MDxx_disp(int mode);
+#define D1_SERI 1
+#define D2_DISP 2
+#define D3_BOTH 3
+extern void prt(String sData, int direction = D3_BOTH);
+extern void prtln(String sData, int direction = D3_BOTH);
+
 ImageViewer viewer;
 
 void forever(void) {
@@ -11,12 +28,28 @@ void forever(void) {
 }
 
 void setup(void) {
+    MODE_ST = MDM1;
     if (!viewer.begin()) {
         forever();
     }
+    MODE_ST = MD00;
 }
 
 void loop(void) {
-    viewer.update();
-    delay(100);
+    M5.update();
+
+    if (MODE_ST == MD00) {
+        if (M5.BtnB.wasHold()) {
+            prtln("BtnB was Hold ,  goto Special Mode", D1_SERI);
+            MODE_ST = MD01;  // Special Mode in
+            MDxx_disp(MODE_ST);
+            delay(100);
+        } else
+            viewer.update();
+
+    } else if ((MODE_ST > MD00) && (MODE_ST <= MD_END))
+        MDxx_BtnChk();
+
+    // delay(100);
+    delay(20);
 }
--- src/ImageViewer.cpp
+++ src/ImageViewer.cpp
@@ -288,7 +288,7 @@ bool ImageViewer::begin(int bgColor) {
 }
 
 bool ImageViewer::update(void) {
-    M5_UPDATE();
+    // M5_UPDATE();
 
     if (this->_isAutoRotation && updateOrientation(GRAVITY_THRESHOLD)) {
         showImage(this->_imageFiles, this->_pos);
@@ -561,6 +561,132 @@ bool ImageViewer::parse(const char* config) {
 }
 
 // -----add by NoRi 2024-06-24 -----------------------------------------------
+#define MDM2   -2    // init
+#define MDM1   -1    // setup start
+#define MD00   0     // setup don (Normal mode)
+#define MD01   1     // MD01  FileSystem change
+#define MD02   2     // MD02  load menu.bin
+#define MD03   3     // MD03  save bin to SD
+#define MD04   4     // MD04  power off
+#define MD_END 4     //
+extern int MODE_ST;  // mode status
+
+void MDxx_BtnChk();
+void doWork(int mode);
+void MD00_disp();
+void MDxx_disp(int mode);
+
+void MDxx_BtnChk() {
+    if (M5.BtnA.wasClicked()) {
+        prtln("BtnA Cliked! [EXIT]", D1_SERI);
+        MD00_disp();
+        MODE_ST = MD00;  // -- normal mode
+        delay(100);
+    } else if (M5.BtnB.wasClicked()) {
+        prtln("BtnB Cliked!  [OK]", D1_SERI);
+        doWork(MODE_ST);
+        MDxx_disp(MODE_ST);
+    } else if (M5.BtnC.wasClicked()) {
+        prtln("BtnC Cliked!  [NEXT]", D1_SERI);
+        MODE_ST++;
+        if (MODE_ST > MD_END)
+            MODE_ST = MD01;
+
+        MDxx_disp(MODE_ST);
+    }
+}
+
+void MD00_disp() {
+    M5.Display.setTextFont(1);
+    M5.Display.setTextSize(2);
+    M5.Display.setTextColor(WHITE, BLACK);
+    M5.Display.setTextDatum(0);
+    M5.Display.setCursor(0, 0);
+    M5.Display.fillScreen(BLACK);
+    M5.Display.setTextScroll(true);
+    M5.Display.printf("***  SDU-imageViewer  ***\n\n\n");
+    M5.Display.printf("(BtnA)click: NEXT image\n\n");
+    M5.Display.printf("(BtnB)hold : Special Mode\n\n");
+    M5.Display.printf("(BtnC)click: PREV image\n");
+}
+
+void MDxx_disp(int mode) {
+    M5.Display.setTextFont(1);
+    M5.Display.setTextSize(2);
+    M5.Display.setTextColor(TFT_WHITE, TFT_BLUE);
+    M5.Display.fillScreen(BLUE);
+    M5.Display.setTextDatum(0);
+    M5.Display.setCursor(0, 0);
+    M5.Display.setTextScroll(false);
+
+    M5.Display.printf("**  Special Mode %d/%d  **\n\n", MODE_ST, MD_END);
+    String msg = "";
+    switch (MODE_ST) {
+        case MD01:
+            // if (WD_FILE_SYSTEM == FS_SD)
+            //     msg = "FileSystem SD -> SPIFFS";
+            // else
+            //     msg = "FileSystem SPIFFS -> SD";
+
+            break;
+
+        case MD02:
+            msg = "Load SD-Updater menu.bin";
+            break;
+
+        case MD03:
+            msg = "Store bin-file to SD";
+            break;
+
+        case MD04:
+            msg = "Power Off";
+            break;
+
+        default:
+            break;
+    }
+    prtln(msg, D2_DISP);
+
+    M5.Display.printf("\n\n\n(BtnA)click:    EXIT\n\n");
+    M5.Display.printf("(BtnB)click:    OK\n\n");
+    M5.Display.print("(BtnC)click:    NEXT\n");
+}
+
+void doWork(int mode) {
+    switch (mode) {
+        case MD01:
+            // M5.Log.println("FileSystem change");
+            // if (WD_FILE_SYSTEM == FS_SD)
+            //     setFileSystemNVM(FS_SPIFFS);
+            // else
+            //     setFileSystemNVM(FS_SD);
+            // REBOOT();
+            break;
+
+        case MD02:
+            M5.Log.println("Will Load menu binary");
+            updateFromFS(SD);
+            for (;;) {
+                delay(10);
+            }
+            // REBOOT();
+            break;
+
+        case MD03:
+            M5.Log.println("Will store BIN_FILE to SD");
+            saveSketchToFS(SD, APP_BIN);
+            delay(500);
+            break;
+
+        case MD04:
+            POWER_OFF();
+            break;
+
+        default:
+            break;
+    }
+}
+
 bool SdBegin() {
     // --- SD begin -------
     int i = 0;
